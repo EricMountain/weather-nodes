215c0037f145352176a36281ab82efa8dd482fe5
Eric Mountain <eric.mountain@gmail.com>
2025-11-16 10:26:16 +0100

    Print target version in Publish OTA target

--- add_target_publish_ota.py
+++ add_target_publish_ota.py
@@ -10,6 +10,9 @@ env.AddCustomTarget(
     name="publish_ota",
     title="Publish OTA",
     dependencies="$BUILD_DIR/${PROGNAME}.bin",
-    actions=f"aws --profile eric s3 cp $SOURCE s3://enry-weather-nodes-fmw-update/{env['PIOENV']}-{short_hash}.bin --region eu-north-1",
+    actions=[
+        f"aws --profile eric s3 cp $SOURCE s3://enry-weather-nodes-fmw-update/{env['PIOENV']}-{short_hash}.bin --region eu-north-1",
+        f"echo 'target_version = {short_hash}'"
+    ],
     always_build=True,
 )
